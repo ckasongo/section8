2dff129fb097a086ea0611ccb0c993df73d2db4a
chill234 <38770933+chill234@users.noreply.github.com>
2019-03-13 15:25:10 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -3,32 +3,18 @@ public class SumSquare {
 public static void main(String[] args) { 
   
    //System.out.print( (3 * 3) +  (6 * 6) + (9 * 9 ) + (12 * 12) + (15 * 15) + (18 * 18) +  (21 * 21) + (24 * 24) +(27 * 27) + ( 30 * 30) +  (33 * 33));
+    int num = 1; 
+   
   
-  System.out.println("Cummulation result: " + ( 3* 1)  +" * " + ( 3* 1)  + " = " + (3 * 3)); 
-    
-  System.out.println("Cummulation result: " + (3 * 2) +" * " + (3 * 2)   + " = " + (6 * 6)); 
+    while (num <= 11){
+  System.out.println("Cummulation result: " + (3 * num)  + " * " + ( 3 * num)  + " = " + (Math.pow((3 * num),2))); 
   
-  System.out.println("Cummulation result: " + (3 * 3) +" * " + (3 * 3)   + " = " + (9 * 9)); 
-  
-  System.out.println("Cummulation result: " + (3 * 4) +" * " + (3 * 4)   + " = " + (12 * 12));
-  
-  System.out.println("Cummulation result: " + (3 * 5) +" * " + (3 * 5)   + " = " + (15 * 15));
-  
-  int num = 1; 
-  
-  while(num <= 11)
-  {
-    
+   num++;  
   }
   
-//While Loop
-  /**
-  while (i % 3) {
-    
-  
-    
-  }
-  **/
-  
+ 
 } 
 } 
+
+
+//sum += (Math.pow((3 * num),2)); 
