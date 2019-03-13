ff2c376d6127e5a1df6b246a3325018f87766b79
chill234 <38770933+chill234@users.noreply.github.com>
2019-03-13 14:36:02 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,1 +1,9 @@
 
+public class SumSquare { 
+public static void main(String[] args) { 
+  
+   System.out.print( (3 * 3) +  (6 * 6) + (9 * 9 ) + (12 * 12) + (15 * 15) + (18 * 18) +  (21 * 21) + (24 * 24) +(27 * 27) + ( 30 * 30) +  (33 * 33))
+    
+
+} 
+} 
