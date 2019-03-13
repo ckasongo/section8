0dea10fb3bedb1b6ab77e5ea0eeed00f837a80f7
ckasongo <48481767+ckasongo@users.noreply.github.com>
2019-03-13 14:54:26 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,8 +1,20 @@
 
 public class SumSquare { 
 public static void main(String[] args) { 
+  int i = 0;
+  while (i < 11){
+    
+    
+    
+    
+    System.out.println("comulation result: ", 3*1);
+    i = i + 1;
+    
+    
   
-   System.out.print( (3 * 3) +  (6 * 6) + (9 * 9 ) + (12 * 12) + (15 * 15) + (18 * 18) +  (21 * 21) + (24 * 24) +(27 * 27) + ( 30 * 30) +  (33 * 33))
+   System.out.print( (3 * 3) +  (6 * 6) + (9 * 9 ) + (12 * 12) + (15 * 15) + (18 * 18) +  
+                    (21 * 21) + (24 * 24) +(27 * 27) + ( 30 * 30) +  (33 * 33));
+  
     
 
 } 
