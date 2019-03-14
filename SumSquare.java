41f49df2cc25e19a2c7897da0b7c2882a0734751
ckasongo <48481767+ckasongo@users.noreply.github.com>
2019-03-13 20:06:32 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,20 +1,20 @@
 
 public class SumSquare { 
-public static void main(String[] args) { 
-  
-   //System.out.print( (3 * 3) +  (6 * 6) + (9 * 9 ) + (12 * 12) + (15 * 15) + (18 * 18) +  (21 * 21) + (24 * 24) +(27 * 27) + ( 30 * 30) +  (33 * 33));
-    int num = 1; 
-   
-  
-    while (num <= 11){
-  System.out.println("Cummulation result: " + (3 * num)  + " * " + ( 3 * num)  + " = " + (Math.pow((3 * num),2))); 
-  
-   num++;  
-  }
+public static void main(String[] args) {
+	int x = 3;
+	int n = 0;
+	while (x <= 33) {
+		n += Math.pow(x, 2);
+		System.out.printf("Cumulation result: %-8d :     %-8d *   %-8d%n", n , x , x);
+		x += 3;
+		}
+	
+		System.out.print("The SUM of Square of multiples is " + n );
+	
+
   
  
-} 
+  } 
 } 
 
 
-//sum += (Math.pow((3 * num),2)); 
