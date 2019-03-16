5f1fb5f949c67d6d1fc7834477f64d982d61163d
ckasongo <48481767+ckasongo@users.noreply.github.com>
2019-03-16 03:04:12 -0400

    Create ForeverStamp.java

---
+++ ForeverStamp.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ForeverStamp {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int count = 0;
+		while (count <= 1) {
+			
+			System.out.print("Enter a sequence of weights in ounces for the letters that will mail out."
+				+ " When done entering, please enter -1 to finish: ");
+			double weight = input.nextDouble();
+			count += 1;
+			System.out.println("You can use a forever stamp");
+			
+			if (weight == -1) {
+				System.out.println("Good Bye");
+			}
+			if (weight > 1) {
+				System.out.println("You need additional postage than a forever stamp");
+			
+			}
+		
+		
+
+		}
+
+
+
+		
+	}
+
+}
